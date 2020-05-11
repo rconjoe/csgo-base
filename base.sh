bfff0bc04fb3254a2ae754fe81645c8ec6d8e233
trogdor the burninator <joe@rconjoe.com>
2020-05-11 14:21:30 -0400

    because we do this often

---
+++ base.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir gs
+cd gs
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install tuned htop
+sudo apt install mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 libstdc++6:i386
+wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh csgoserver
+./csgoserver auto-install
