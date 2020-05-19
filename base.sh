94a931f2450ec469147d1e771fad405d44bdf92b
trogdor the burninator <joe@rconjoe.com>
2020-05-19 15:05:21 -0400

    change wget path to custom builder

--- base.sh
+++ base.sh
@@ -6,5 +6,5 @@ sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install tuned htop
 sudo apt install mailutils postfix curl wget file bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux lib32gcc1 libstdc++6 libstdc++6:i386
-wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh csgoserver
+wget -O linuxgsm.sh https://raw.githubusercontent.com/rconjoe/lgsm-plus/master/linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh csgoserver
 ./csgoserver auto-install
